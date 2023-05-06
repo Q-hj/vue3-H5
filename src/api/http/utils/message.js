771732b76b30b206f9448e187797c4928d997899
Q-hj <1485642186@qq.com>
2023-05-06 14:30:32 +0800

    feat: ✨ 配置vant4反馈组件

--- src/api/http/utils/message.js
+++ src/api/http/utils/message.js
@@ -1,14 +1,22 @@
 // * 统一消息提醒组件的调用
 
+// import { ElMessage } from 'element-plus';
 // import { Message, Modal, ModalConfig } from "@arco-design/web-vue";
 // import { Toast } from 'vant';//vant3-vue2
 import { showToast, showSuccessToast, showFailToast } from 'vant'; //vant4-vue3
 
-// import { ElMessage } from 'element-plus';
+const vant4Message = (type, message) =>
+	showToast({
+		type,
+		message,
+		iconSize: '2em',
+		duration: 1500,
+		// wordBreak
+	});
 
 const Message = {
-	success: showSuccessToast,
-	error: showFailToast,
+	success: (message) => vant4Message('success', message),
+	error: (message) => vant4Message('fail', message),
 };
 
 //  const message = {
