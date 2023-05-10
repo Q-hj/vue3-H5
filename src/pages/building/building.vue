7b28d47227af7e0384316366d668a50f53d7cf60
Q-hj <1485642186@qq.com>
2023-05-10 09:31:32 +0800

    feat(建设页): 💄 新增适老化

--- src/pages/building/building.vue
+++ src/pages/building/building.vue
@@ -1,4 +1,7 @@
-<script setup></script>
+<script setup>
+	import { useGlobalStore } from '@/stores';
+	const { isOlderUI } = useGlobalStore();
+</script>
 
 <template>
 	<section class="h-full flex-center">
@@ -8,7 +11,12 @@
 				src="@/assets/images/building.png"
 				alt=""
 			/>
-			<p class="text-12px color-#CCCCCC">页面建设中，敬请期待！</p>
+			<p
+				:class="isOlderUI ? 'text-20px' : 'text-12px'"
+				class="color-#CCCCCC"
+			>
+				页面建设中，敬请期待！
+			</p>
 		</div>
 	</section>
 </template>
